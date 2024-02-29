ae978bd9ad29db4a4e307fb2664a9e5639de0c16
gaoqiang <gaoqiang@yundun119.com>
2024-02-29 13:22:04 +0800

    样式修改

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import router from './router/routes'
 import App from './App.vue'
 import '@renderer/assets/css/tailwind.css'
+import '@renderer/assets/css/global.css'
 // If there is a problem with the style, it may be a conflict between tailwind and element
 // import 'element-plus/dist/index.css'
 import piniaPersist from 'pinia-plugin-persist'
