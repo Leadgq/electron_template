ecf579f40b13090f1952dac4ab9d2a78a0238537
gaoqiang <gaoqiang@yundun119.com>
2025-08-22 10:11:53 +0800

    fix:修改默認指向、否則build之后不显示任何内容

--- src/renderer/src/router/routes.js
+++ src/renderer/src/router/routes.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
+      path: '/:any(.*)*',
       name: 'Home',
       component: () => import('../views/Home.vue')
     },
